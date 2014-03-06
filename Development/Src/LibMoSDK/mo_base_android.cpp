9ad98e305e252c28bf79bd2d7cde20d77ba4457d
gamemake <gamemake@gmail.com>
2014-03-06 20:57:14 +0800

    add mosdk android support

---
+++ Development/Src/LibMoSDK/mo_base_android.cpp
@@ -0,0 +1,35 @@
+#ifdef ANDROID
+
+#include <ZionSTL.h>
+#include "mosdk.h"
+
+void MOInit(const char* appname)
+{
+}
+
+void MOFini()
+{
+
+}
+
+const char* MOGetDeviceUDID()
+{
+	return NULL;
+}
+
+const char* MOGetAppName()
+{
+	return NULL;
+}
+
+const char* MOGetOSName()
+{
+	return NULL;
+}
+
+const char* MoGetResourcePath()
+{
+	return NULL;
+}
+
+#endif
---
+++ Development/Src/LibMoSDK/mo_request_android.cpp
@@ -0,0 +1,49 @@
+#ifdef ANDROID
+
+#include <ZionSTL.h>
+#include "mosdk.h"
+
+void MOEnableDebug(bool enable)
+{
+}
+
+MOREQUEST* MORequestString(const char* url, const Zion::Map<Zion::String, Zion::String>& params)
+{
+	return NULL;
+}
+
+MOREQUEST* MODownloadFile(const char* url, const Zion::Map<Zion::String, Zion::String>& params, const char* path, bool append)
+{
+	return NULL;
+}
+
+MOREQUEST* MORequestString(const char* url, const char* postdata)
+{
+	return NULL;
+}
+
+MOREQUEST* MODownloadFile(const char* url, const char* postdata, const char* path, bool append)
+{
+	return NULL;
+}
+
+void MORequestDestory(MOREQUEST* request)
+{
+}
+
+MOREQUESTSTATE MORequestStatus(MOREQUEST* request)
+{
+	return MOREQUESTSTATE_DONE;
+}
+
+const char* MORequestGetResult(MOREQUEST* request)
+{
+	return NULL;
+}
+
+int MORequestGetResultLength(MOREQUEST* request)
+{
+	return 0;
+}
+
+#endif

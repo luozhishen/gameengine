a16ec225a4dc33f747d94f99e714d0e8862c9a25
gamemake <gamemake@gmail.com>
2014-03-09 12:07:45 +0800

    added parse args for datacache

--- Development/Src/LibDataCache/DataCache.cpp
+++ Development/Src/LibDataCache/DataCache.cpp
@@ -20,9 +20,58 @@ namespace Zion
 		String CONFIG_DATABASE("sqlite:zion_db.sqlite");
 		String CONFIG_RPCEP("0.0.0.0:1982");
 
+		static bool ParseArgs(int argc, char* argv[])
+		{
+			for(int i=1; i<argc; i++)
+			{
+				String name, value;
+				char* pos = strchr(argv[i], '=');
+				if(!pos) pos = argv[i] + strlen(argv[i]);
+				char* pos1 = strchr(argv[i], ':');
+				if(!pos1 && pos1<pos) pos = pos1;
+				name.copy(argv[i], pos1-argv[i]);
+				if(*pos!='\0') value = pos + 1;
+
+				if(name=="-simple" && value=="off")
+				{
+					CONFIG_SIMPLE_MODE = false;
+					continue;
+				}
+				if(name=="-db")
+				{
+					CONFIG_DATABASE = value;
+					continue;
+				}
+				if(name=="-rpcep")
+				{
+					CONFIG_RPCEP = value;
+					continue;
+				}
+
+				return false;
+			}
+			return true;
+		}
+
 		int Main(int argc, char* argv[])
 		{
 			// step 1: parse command line
+			if(!ParseArgs(argc, argv))
+			{
+				printf("invalid parameter\n");
+				printf("\n");
+				printf("Usage : %s\n", argv[0]);
+				printf("		-simple=[on|off]\n");
+				printf("		-db=[database string]\n");
+				printf("		-rpcep=[127.0.0.1:1982]\n");
+				return -1;
+			}
+
+			printf("CONFIG :\n");
+			printf("	SIMPLE_MODE = %s\n", CONFIG_SIMPLE_MODE?"true":"false");
+			printf("	DATABASE = %s\n", CONFIG_DATABASE.c_str());
+			printf("	RPCEP = %s\n", CONFIG_RPCEP.c_str());
+			printf("\n");
 
 			// step 2: load content
 			if(!CONFIG_SIMPLE_MODE)

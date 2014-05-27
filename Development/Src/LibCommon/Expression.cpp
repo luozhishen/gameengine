37a20f06fa6b43a990705e4af61f825e6982c12f
gamemake <gamemake@gmail.com>
2014-05-28 00:20:50 +0800

    added expression

---
+++ Development/Src/LibCommon/Expression.cpp
@@ -0,0 +1,142 @@
+#ifndef __ZION_EXPRESSION__
+#define __ZION_EXPRESSION__
+
+namespace Zion
+{
+	namespace Expression
+	{
+
+		class BaseNode;
+		class Node;
+		class VM;
+
+		class BaseNode
+		{
+		public:
+			virtual void GetValue(VM* vm, JsonValue& value) = 0;
+			void SetParent(Node* pParent, _U32 Index);
+
+		private:
+			Node* m_pParent;
+			_U32 m_Index;
+		};
+
+		class ConstantNode : public BaseNode
+		{
+		};
+
+		class VariantNode : public BaseNode
+		{
+		};
+
+		class Node : public CBaseNode
+		{
+		public:
+			Node(_U32 level);
+
+			void Apend(BaseNode* node);
+			void SetNode(_U32 index, BaseNode* node);
+			_U32 GetNodeCount();
+			BaseNode* GetNode(_U32 index);
+
+		private:
+			Array<BaseNode*> m_Children;
+		};
+
+		// level 0
+		class FunctionNode : public Node
+		{
+		public:
+			FunctionNode(const String& name) : Node(0) {}
+
+		private:
+			String m_Name;
+		};
+
+		class NotNode : public Node
+		{
+		};
+
+		class BracketNode : public Node
+		{
+		};
+
+		class ArrayNode : public Node
+		{
+		public:
+		private:
+			String m_Name;
+		};
+
+		// level 1
+		class MultiplyNode : public Node
+		{
+		};
+
+		class DivideNode : public Node
+		{
+		};
+
+		class ModNode : public Node
+		{
+		};
+
+		// level 2
+		class AddNode : public Node
+		{
+		};
+
+		class SubNode : public Node
+		{
+		};
+
+		// level 3
+		class EqualNode : public Node
+		{
+		};
+
+		class NotEqualNode : public Node
+		{
+		};
+
+		class GreaterNode : public Node
+		{
+		};
+
+		class LessNode : public Node
+		{
+		};
+
+		class GreaterEqualNode : public Node
+		{
+		};
+
+		class LessEqualNode : public Node
+		{
+		};
+
+		// level 4
+		class AndNode : public Node
+		{
+		};
+
+		class OrNode : public Node
+		{
+		};
+
+		//
+		class VM
+		{
+		public:
+			virtual bool GetVariant(const String& name, JsonValue& value) = 0;
+			virtual bool GetArray(const String& name, _U32 index, JsonValue& value) = 0;
+			virtual bool CallFunction(const String& name, const Array<JsonValue>& params, JsonValue& value) = 0;
+		};
+
+		bool ParseExpression(const String& exp, BracketNode& node);
+		bool ExecuteExpression(VM* vm, const BraketNode& node, JsonValue& value);
+
+	}
+}
+
+#endif

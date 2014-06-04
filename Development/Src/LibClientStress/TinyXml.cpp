69624bd273d3bb3df21ef1a2154ce7c8a92d2dca
gamemake <gamemake@gmail.com>
2014-06-04 16:47:32 +0800

    mm

--- Development/Src/LibClientStress/TinyXml.cpp
+++
@@ -1,4 +0,0 @@
-#include "../../External/TinyXml/tinystr.cpp"
-#include "../../External/TinyXml/tinyxml.cpp"
-#include "../../External/TinyXml/tinyxmlerror.cpp"
-#include "../../External/TinyXml/tinyxmlparser.cpp"
